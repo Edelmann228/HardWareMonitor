ccca7dfb38f797adbf06ab8a3b2118c238822609
Adxia07 <Alex.s.0705@mail.ru>
2026-02-21 03:44:08 +0300

    добавил класс MainWindow.xaml.cs

--- HardWareMonitor/Views/MainWindow.xaml.cs
+++ HardWareMonitor/Views/MainWindow.xaml.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System.Windows;
+using HardWareMonitor.ViewModels;
+
+namespace HardWareMonitor.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Можно добавить дополнительную инициализацию если нужно
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Действия при загрузке окна
+            var viewModel = DataContext as MainViewModel;
+            if (viewModel != null)
+            {
+                // Можно вызвать что-то при загрузке
+            }
+        }
+    }
+}
