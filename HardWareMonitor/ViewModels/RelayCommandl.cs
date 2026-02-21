d12fd9463abd03b5202723571c6ad24b126e179b
Adxia07 <Alex.s.0705@mail.ru>
2026-02-21 03:41:00 +0300

    добавил класс RelayCommand

---
+++ HardWareMonitor/ViewModels/RelayCommandl.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace HardWareMonitor.ViewModels
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public RelayCommand(Action execute, Func<bool> canExecute = null)
+            : this(p => execute(), p => canExecute())
+        {
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
